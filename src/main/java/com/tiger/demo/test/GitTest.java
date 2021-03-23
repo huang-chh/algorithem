1813144c13a21e8158cc8f84345f85be76d5fad3
huangchuanhu <huangchuanhu@jd.com>
2021-03-23 09:57:40 +0800

    配置文件修改

---
+++ src/main/java/com/tiger/demo/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.tiger.demo.test;
+
+/**
+ * @author huangchuanhu
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

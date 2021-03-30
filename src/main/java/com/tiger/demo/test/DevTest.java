c785649922229e0c2e22e58687f26033a5f9a06d
huangchuanhu <huangchuanhu@jd.com>
2021-03-30 20:56:22 +0800

    暂存dev文件

---
+++ src/main/java/com/tiger/demo/test/DevTest.java
@@ -0,0 +1,10 @@
+package com.tiger.demo.test;
+
+/**
+ * @author huangchuanhu
+ */
+public class DevTest {
+    public static void main(String[] args) {
+        System.out.println("hello dev");
+    }
+}
